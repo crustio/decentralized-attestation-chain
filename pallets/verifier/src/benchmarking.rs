594dd4a04eac87ab391aa49d12109fd1dc5990f7
Kun <zikunvan@gmail.com>
2021-06-20 22:16:59 +0800

    Init proj (#1)

---
+++ pallets/verifier/src/benchmarking.rs
@@ -0,0 +1,24 @@
+//! Benchmarking setup for pallet-verifier
+
+use super::*;
+
+use frame_system::RawOrigin;
+use frame_benchmarking::{benchmarks, whitelisted_caller, impl_benchmark_test_suite};
+#[allow(unused)]
+use crate::Pallet as Verifier;
+
+benchmarks! {
+	do_something {
+		let s in 0 .. 100;
+		let caller: T::AccountId = whitelisted_caller();
+	}: _(RawOrigin::Signed(caller), s)
+	verify {
+		assert_eq!(Something::<T>::get(), Some(s));
+	}
+}
+
+impl_benchmark_test_suite!(
+	Verifier,
+	crate::mock::new_test_ext(),
+	crate::mock::Test,
+);
